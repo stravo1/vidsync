01765f7613e94897e80b3ed77f23fa89a63ae8f2
stravo1@msi <stravoone@gmail.com>
2022-12-24 12:41:23 +0530

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import TopPanel from "./lib/TopPanel.svelte";
+</script>
+
+<main class="background">
+  <TopPanel />
+</main>
+
+<style>
+  main {
+    box-sizing: border-box;
+    height: 100%;
+    padding: 1rem;
+  }
+</style>
---
+++ src/lib/TopPanel.svelte
@@ -0,0 +1,19 @@
+<header class="on-background-text">
+  <h1>vidsync</h1>
+  <section>
+    <span class="material-symbols-rounded"> light_mode </span>
+    <a href="/" title="code"><span class="material-symbols-rounded"> code </span></a>
+  </section>
+</header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  section{
+    display: flex;
+    gap: 0 2rem;
+  }
+</style>
