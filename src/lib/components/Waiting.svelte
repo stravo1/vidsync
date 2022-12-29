b198dd6ca6e245e0aa62f44a115ee00b48261780
stravo1@msi <stravoone@gmail.com>
2022-12-29 20:50:46 +0530

    add: added automatic clipboard copy

--- src/lib/components/Waiting.svelte
+++ src/lib/components/Waiting.svelte
@@ -1,9 +1,14 @@
 <script>
   import { createEventDispatcher } from "svelte";
+  import { onMount } from "svelte";
   import connect from "../../assets/images/14.svg";
+  import ClipboardJS from "clipboard/dist/clipboard.min.js";
 
   let dispatch = createEventDispatcher();
   export let roomId;
+  onMount(() => {
+    new ClipboardJS(".roomIdButton");
+  });
 
   const hangup = () => {
     var sure = confirm("Abandon room?");
@@ -20,8 +25,18 @@
   <section class="on-surface-variant-text info">
     <h2>waiting...</h2>
     room id:
-    <section class="roomId on-surface-text">
-      {roomId}
+    <section
+      id="copy"
+      data-clipboard-text={roomId}
+      class="roomIdHolder on-surface-text"
+    >
+      <div class="roomId">{roomId}</div>
+      <button
+        data-clipboard-text={roomId}
+        class="roomIdButton surface-variant on-surface-variant-text"
+      >
+        <span class="material-symbols-rounded "> copy_all </span>
+      </button>
     </section>
     <button on:click={hangup} class="center error on-error-text">
       hang up
@@ -48,14 +63,25 @@
     margin: 1rem 0;
     text-align: center;
   }
-  .roomId {
-    padding: 0.5rem 0.25rem;
+  .roomIdHolder {
+    display: flex;
     margin: 0.25rem 0;
     word-break: break-all;
     user-select: all;
     border-radius: 0.25rem;
     border: 2px #cac4d0;
     border-style: dashed;
+    font-size: small;
+    padding: 0.5rem;
+  }
+  .roomId {
+    flex-basis: 90%;
+    padding: 0.5rem 0.25rem;
+  }
+  .roomIdButton {
+    width: 2rem;
+    margin: 0;
+    padding: 0;
     font-size: small;
   }
   button {
